cf7915e188fbec71539a1c094b8f3066140c6528
전지훈 <junji636@gmail.com>
2022-11-12 22:57:17 +0900

    home 화면 작업

--- swiftUI_heroAnimation/swiftUI_heroAnimation/View/Home.swift
+++ swiftUI_heroAnimation/swiftUI_heroAnimation/View/Home.swift
@@ -1,16 +1,17 @@
 //
 //  Home.swift
-//  swiftUI_heroAnimation
-//
-//  Created by 전지훈 on 2022/11/10.
 //
 
 import SwiftUI
 
 struct Home: View {
+    //MARK: Animation Properties
+    @State var currentItem: Today?
+    @State var showDetailPage: Bool = false
+    
     var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
-            VStack(spacing: 30) {
+            VStack(spacing: 0) {
                 HStack(alignment: .bottom) {
                     VStack(alignment: .leading, spacing: 8) {
                         Text("Monday 4 April")
@@ -30,8 +31,101 @@ struct Home: View {
                     }
                 }
                 .padding(.horizontal)
+                .padding(.bottom)
+                
+                ForEach(todayItems) { item in
+                    Button {
+                        withAnimation(.interactiveSpring(response: 0.6, dampingFraction: 0.7, blendDuration: 0.7)) {
+                            currentItem = item
+                            showDetailPage = true
+                        }
+                    } label: {
+                        cardView(item: item)
+                            .scaleEffect(currentItem?.id == item.id ? 1 : 0.93)
+                    }
+                    .buttonStyle(ScaledButtonStyle())
+                }
             }
             .padding(.vertical)
+        }
+    }
+}
+
+extension Home {
+    @ViewBuilder
+    func cardView(item: Today) -> some View {
+        VStack(alignment: .leading, spacing: 15) {
+            ZStack(alignment: .topLeading) {
+                GeometryReader { proxy in
+                    let size = proxy.size
+                    Image(item.artWork)
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: size.width, height: size.height)
+                        .clipShape(CustomCorner(corners: [.topLeft, .topRight], radius: 15))
+                }
+                .frame(height: 400)
+                
+                LinearGradient(colors: [
+                    .black.opacity(0.5),
+                    .black.opacity(0.2),
+                    .clear
+                ], startPoint: .top, endPoint: .bottom)
+                
+                VStack(alignment: .leading, spacing: 8) {
+                    Text(item.platformTitle.uppercased())
+                        .font(.callout)
+                    
+                    Text(item.bannerTitle)
+                        .font(.largeTitle.bold())
+                        .multilineTextAlignment(.leading)
+                }
+                .foregroundColor(.primary)
+                .padding()
+            }
+            
+            HStack(spacing: 12) {
+                Image(item.appLogo)
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 60, height: 60)
+                    .clipShape(RoundedRectangle(cornerRadius: 15, style: .continuous))
+                
+                VStack(alignment: .leading, spacing: 4) {
+                    Text(item.platformTitle.uppercased())
+                        .font(.caption)
+                        .foregroundColor(.gray)
+                    
+                    Text(item.appName)
+                        .fontWeight(.bold)
+                    
+                    Text(item.appDescription)
+                        .font(.caption)
+                        .foregroundColor(.gray)
+                }
+                .foregroundColor(.primary)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                
+                Button {
+                    
+                } label: {
+                    Text("GET")
+                        .fontWeight(.bold)
+                        .foregroundColor(.blue)
+                        .padding(.vertical, 8)
+                        .padding(.horizontal, 20)
+                        .background {
+                            Capsule()
+                                .fill(.ultraThinMaterial)
+                        }
+                }
+
+            }
+            .padding([.horizontal, .bottom])
+        }
+        .background {
+            RoundedRectangle(cornerRadius: 15, style: .continuous)
+                .fill(Color(.systemGray5))
         }
     }
 }
@@ -40,5 +134,13 @@ struct Home_Previews: PreviewProvider {
     static var previews: some View {
         Home()
             .preferredColorScheme(.dark)
+    }
+}
+
+struct ScaledButtonStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .scaleEffect(configuration.isPressed ? 0.94 : 1)
+            .animation(.easeOut, value: configuration.isPressed)
     }
 }
